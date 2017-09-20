cecb5322b51c12e21c5165fcc41201026c36b17c
Tianhao Gu <nirvanagth@gmail.com>
2017-09-20 00:31:08 -0700

    weather app done

---
+++ playground/promise-2.js
@@ -0,0 +1,33 @@
+const request = require('request')
+var geocodeAddress = (address) => {
+    return new Promise((resolve, reject) => {
+        var encodedAddress = encodeURIComponent(address)
+
+        request({
+            url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+            json: true
+        }, (error, response, body) => {
+            // console.log(JSON.stringify(body, undefined, 2))
+            if (error) {
+                reject('Unable to connect to Google servers')
+                // console.log('Unable to connect to Google servers')
+            } else if (body.status === 'ZERO_RESULTS') {
+                reject('Unable to find the address')
+                // console.log('Unable to find the address')
+            } else if (body.status === 'OK') {
+                resolve({
+                    address: body.results[0].formatted_address,
+                    latitude: body.results[0].geometry.location.lat,
+                    longitude: body.results[0].geometry.location.lng
+                })
+
+            }
+        })
+    })
+}
+
+geocodeAddress('000000').then((location) => {
+    console.log(JSON.stringify(location, undefined, 2))
+}, (errorMessage) => {
+    console.log(errorMessage)
+})
